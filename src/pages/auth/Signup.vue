d06b72e999e913a973d385386211f94c60dab5e6
PH7-Jack <pedrolivertwd@gmail.com>
2020-05-09 20:02:53 -0300

    UPDATE signup page

--- src/pages/auth/Signup.vue
+++ src/pages/auth/Signup.vue
@@ -1,11 +1,39 @@
 <template>
-  <q-page padding>
-    <!-- content -->
-  </q-page>
+  <div>
+    <q-card flat class="bg-transparent">
+      <q-card-section>
+        <q-form class="q-gutter-sm">
+          <qx-input label="Nome Completo" v-model="name" />
+          <qx-input label="Email" v-model="email" />
+          <qx-input label="Username" v-model="username" />
+          <qx-password label="Senha" v-model="password" />
+          <qx-password label="Confirme sua Senha" v-model="password" />
+        </q-form>
+      </q-card-section>
+
+      <q-card-actions class="px-40" align="center">
+        <qx-btn label="Criar Conta" class="full-width" />
+      </q-card-actions>
+    </q-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'PageSignup'
+  name: 'PageSignup',
+
+  components: {
+    QxInput: () => import('components/input/QxInput'),
+    QxPassword: () => import('components/input/QxPassword'),
+    QxBtn: () => import('components/button/QxBtn')
+  },
+  data () {
+    return {
+      name: '',
+      email: '',
+      username: '',
+      password: ''
+    }
+  }
 }
 </script>
