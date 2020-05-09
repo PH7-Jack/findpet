69e02fb04e196767eecd56abdb01d8daa4b327e9
PH7-Jack <pedrolivertwd@gmail.com>
2020-05-09 19:55:44 -0300

    UPDATE login page

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,6 +1,16 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="lHh Lpr lFf" class="flex flex-center">
     <q-page-container>
+      <div class="text-center">
+        <q-img width="200px" src="/statics/images/logo.svg" />
+        <div>
+          <p class="fs-16 text-grey-6 mt-10">
+            Encontre seu novo pet <br />
+            ou dõe um novo amiguinho <br />
+            para alguém
+          </p>
+        </div>
+      </div>
       <router-view />
     </q-page-container>
   </q-layout>
--- src/pages/auth/Login.vue
+++ src/pages/auth/Login.vue
@@ -1,11 +1,34 @@
 <template>
-  <q-page padding>
-    <!-- content -->
-  </q-page>
+  <div>
+    <q-card flat class="bg-transparent">
+      <q-card-section>
+        <q-form class="q-gutter-sm">
+          <qx-input label="Email" v-model="email" />
+          <qx-password label="Senha" v-model="password" />
+        </q-form>
+      </q-card-section>
+
+      <q-card-actions class="px-40" align="center">
+        <qx-btn label="Login" class="full-width" />
+      </q-card-actions>
+    </q-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'PageLogin'
+  name: 'PageLogin',
+
+  components: {
+    QxInput: () => import('components/input/QxInput'),
+    QxPassword: () => import('components/input/QxPassword'),
+    QxBtn: () => import('components/button/QxBtn')
+  },
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  }
 }
 </script>
