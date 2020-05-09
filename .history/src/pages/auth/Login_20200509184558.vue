ba069a1c9190b08e76b98b6e3677300e367149eb
PH7-Jack <pedrolivertwd@gmail.com>
2020-05-09 18:48:34 -0300

    CHANGE quasar colors

---
+++ .history/src/pages/auth/Login_20200509184558.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageLogin',
+
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
