9a0a6ed59fa70f8b83a6a982697b381dc4469f25
PH7-Jack <pedrolivertwd@gmail.com>
2020-05-10 18:57:50 -0300

    ADD QxSearch component

---
+++ src/components/input/QxSearch.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="bg-grey-2 round-7 overflow-hidden pl-10">
+    <q-input
+      @input="emit"
+      borderless
+      placeholder="Pesquisar"
+      dense
+      :debounce="debounce"
+      v-model="model"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QxSearch',
+
+  props: {
+    debounce: {
+      type: Number,
+      default: 1000
+    }
+  },
+  data () {
+    return {
+      model: ''
+    }
+  },
+  methods: {
+    emit () {
+      this.$emit('input', this.model)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.q-field--focused {
+  background: transparent !important;
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,18 @@
 <template>
   <q-page padding>
-    <adopt-card />
+    <q-toolbar class="bg-white round-8">
+      <q-btn
+        class="relative"
+        style="left: -5px"
+        color="grey-8"
+        icon="eva-funnel-outline"
+        flat
+        round
+      />
+      <qx-search class="full-width" />
+    </q-toolbar>
+
+    <adopt-card v-for="n in 10" :key="n" />
   </q-page>
 </template>
 
@@ -9,7 +21,8 @@ export default {
   name: 'PageIndex',
 
   components: {
-    AdoptCard: () => import('components/card/AdoptCard')
+    AdoptCard: () => import('components/card/AdoptCard'),
+    QxSearch: () => import('components/input/QxSearch')
   }
 }
 </script>
