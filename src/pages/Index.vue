8d6b2c1a5660e4c00168fe826db9b4045db1a023
PH7-Jack <pedrolivertwd@gmail.com>
2020-05-10 21:24:39 -0300

    ADD filter layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,7 +8,68 @@
         icon="eva-funnel-outline"
         flat
         round
-      />
+      >
+        <q-menu content-class="p-3 vx-shadow round-10">
+          <q-card style="width: 250px" class="vx-shadow round-10 text-grey-9">
+            <q-toolbar>
+              <q-toolbar-title class="fs-19 text-weight-light">
+                Filtros
+              </q-toolbar-title>
+              <q-btn flat round dense v-close-popup icon="eva-close-outline" />
+            </q-toolbar>
+            <q-separator inset />
+            <q-card-section class="q-gutter-sm">
+              <q-select
+                dense
+                filled
+                v-model="filter.pets"
+                option-label="name"
+                option-value="id"
+                multiple
+                :options="pets"
+                label="Estou procurando por"
+              />
+              <q-select
+                dense
+                filled
+                v-model="filter.years"
+                multiple
+                :options="years"
+                label="Com a idade"
+              />
+              <q-select
+                dense
+                filled
+                v-model="filter.colors"
+                multiple
+                :options="colors"
+                label="Com a Cor"
+              />
+              <div>
+                <span>
+                  Procurar em
+                  <b>{{ filter.proximity }}</b>
+                  Km
+                </span>
+                <q-slider
+                  v-model="filter.proximity"
+                  :min="0"
+                  :max="30"
+                  :step="1"
+                  label
+                />
+              </div>
+              <qx-btn
+                dense
+                color="primary"
+                class="full-width"
+                label="Salvar"
+                v-close-popup
+              />
+            </q-card-section>
+          </q-card>
+        </q-menu>
+      </q-btn>
       <qx-search class="full-width" />
     </q-toolbar>
 
@@ -22,7 +83,21 @@ export default {
 
   components: {
     AdoptCard: () => import('components/card/AdoptCard'),
-    QxSearch: () => import('components/input/QxSearch')
+    QxSearch: () => import('components/input/QxSearch'),
+    QxBtn: () => import('components/button/QxBtn')
+  },
+  data () {
+    return {
+      filter: {
+        pets: [{ id: 1, name: 'Gato' }, { id: 2, name: 'Cachorro' }],
+        years: [1, 2, 3],
+        colors: ['Preto', 'Pardo', 'Branco', 'Amarelo', 'Malhado'],
+        proximity: 3
+      },
+      pets: [{ id: 1, name: 'Gato' }, { id: 2, name: 'Cachorro' }],
+      years: [1, 2, 3],
+      colors: ['Preto', 'Pardo', 'Branco', 'Amarelo', 'Malhado']
+    }
   }
 }
 </script>
