ba056bf2bc0ad8a3a217f815b203c351005088e3
Carlos Santos <carlos.santos74@outlook.com>
2020-05-11 23:32:37 -0300

    ADD: social login component

---
+++ src/components/button/SocialLogin.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="social-login">
+        <div class="col-6 text-center">
+            <q-btn
+              size="20px"
+              color="$primary"
+              icon="eva-facebook-outline"
+            />
+        </div>
+        <div class="col-6 text-center">
+            <q-btn
+              size="20px"
+              color="$primary"
+              icon="eva-google-outline"
+            />
+          </div>
+</template>
+
+<script>
+export default {
+  name: 'SocialLogin',
+  data () {
+    //
+  }
+}
+</script>
