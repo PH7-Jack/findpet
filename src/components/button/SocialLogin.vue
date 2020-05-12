53b975d9c590937dc7bd82e1017c233b6cfcfdac
PH7-Jack <pedrolivertwd@gmail.com>
2020-05-12 12:47:42 -0300

    FIX errors

--- src/components/button/SocialLogin.vue
+++ src/components/button/SocialLogin.vue
@@ -1,27 +1,21 @@
 <template>
   <div>
     <div class="social-login">
-        <div class="col-6 text-center">
-            <q-btn
-              size="20px"
-              color="$primary"
-              icon="eva-facebook-outline"
-            />
-        </div>
-        <div class="col-6 text-center">
-            <q-btn
-              size="20px"
-              color="$primary"
-              icon="eva-google-outline"
-            />
-          </div>
+      <div class="col-6 text-center">
+        <q-btn size="20px" color="$primary" icon="eva-facebook-outline" />
+      </div>
+      <div class="col-6 text-center">
+        <q-btn size="20px" color="$primary" icon="eva-google-outline" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'SocialLogin',
   data () {
-    //
+    return {}
   }
 }
 </script>
